459e9ac99163c76f4ba465339f2f7c522d71ae78
Kyungmin Kim <kyminbb.kim@gmail.com>
2024-08-17 19:52:05 -0400

    Add `Nth`

--- it.go
+++ it.go
@@ -21,3 +21,18 @@ func Count[V any](seq iter.Seq[V]) int {
 	}
 	return count
 }
+
+// Nth returns the n-th element of seq. The second return value reports whether the element exists.
+//
+// The count is zero-based, so Nth(seq, 0) returns the first element.
+func Nth[V any](seq iter.Seq[V], n int) (V, bool) {
+	i := 0
+	for v := range seq {
+		if i == n {
+			return v, true
+		}
+		i++
+	}
+	var zero V
+	return zero, false
+}
