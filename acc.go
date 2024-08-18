a329aadfb5d9d528d74107d03ade3c00ebd5fb44
Kyungmin Kim <kyminbb.kim@gmail.com>
2024-08-17 23:01:51 -0400

    Add `Min`

--- acc.go
+++ acc.go
@@ -11,10 +11,22 @@ func Max[V cmp.Ordered](seq iter.Seq[V]) (V, bool) {
 	var max V
 	ok := false
 	for v := range seq {
-		ok = true
-		if v > max {
-			max = v
+		if !ok || v > max {
+			max, ok = v, true
 		}
 	}
 	return max, ok
+}
+
+// Min returns the minimum element of seq.
+// The second return value reports whether the element exists (seq is not empty).
+func Min[V cmp.Ordered](seq iter.Seq[V]) (V, bool) {
+	var min V
+	ok := false
+	for v := range seq {
+		if !ok || v < min {
+			min, ok = v, true
+		}
+	}
+	return min, ok
 }
