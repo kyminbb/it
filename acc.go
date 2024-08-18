044c0da43eb50dc0775d8248c0091e40634d4f8f
Kyungmin Kim <kyminbb.kim@gmail.com>
2024-08-17 23:18:22 -0400

    Add `Fold`

--- acc.go
+++ acc.go
@@ -8,25 +8,37 @@ import (
 // Max returns the maximum element of seq.
 // The second return value reports whether the element exists (seq is not empty).
 func Max[V cmp.Ordered](seq iter.Seq[V]) (V, bool) {
-	var max V
+	var acc V
 	ok := false
 	for v := range seq {
-		if !ok || v > max {
-			max, ok = v, true
+		if !ok || v > acc {
+			acc, ok = v, true
 		}
 	}
-	return max, ok
+	return acc, ok
 }
 
 // Min returns the minimum element of seq.
 // The second return value reports whether the element exists (seq is not empty).
 func Min[V cmp.Ordered](seq iter.Seq[V]) (V, bool) {
-	var min V
+	var acc V
 	ok := false
 	for v := range seq {
-		if !ok || v < min {
-			min, ok = v, true
+		if !ok || v < acc {
+			acc, ok = v, true
 		}
 	}
-	return min, ok
+	return acc, ok
+}
+
+// Fold applies f to each element of seq and returns the accumulated result.
+//
+// init is the initial value of the accumulator.
+// The result of each call to f becomes the accumulator for the next call.
+func Fold[V, B any](seq iter.Seq[V], init B, f func(B, V) B) B {
+	acc := init
+	for v := range seq {
+		acc = f(acc, v)
+	}
+	return acc
 }
