390fcffcb4043efe1869758923b9c76234d008c3
Kyungmin Kim <kyminbb.kim@gmail.com>
2024-09-07 17:46:28 -0400

    Add `All2`

--- seq2.go
+++ seq2.go
@@ -2,6 +2,17 @@ package it
 
 import "iter"
 
+// All2 returns an iterator over all key-value pairs of m.
+func All2[K comparable, V any](m map[K]V) iter.Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		for k, v := range m {
+			if !yield(k, v) {
+				return
+			}
+		}
+	}
+}
+
 // Zip returns an iterator over pairs of elements from seqV and seqW.
 //
 // The returned iterator stops when either seqV or seqW stops.
